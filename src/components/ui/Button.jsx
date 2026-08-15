4622f75aeefbde77bc3d43038863f124bb3e45cb
DirectorofWealth <reverencewealth005@gmail.com>
2026-08-15 23:07:23 +0100

    created the Button.jsx reusable component

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,17 @@
+export default function Button(){
+
+    return(
+        <div>
+
+        <button
+        type="button"
+        className="shrink-0 rounded-full bg-black px-6 py-3 text-xs font-medium text-white transition-transform duration-200 ease-in-out 
+        hover:-translate-y-1  cursor-pointer ring focus:ring-1 focus:ring-gray-900 shadow-md"
+      >
+        Get Your Agent
+      </button>
+
+        </div>
+        
+    )
+}
