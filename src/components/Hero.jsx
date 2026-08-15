72d765aaf5b3aaae2dfeba1380785d8a7c54a64a
Favour <kiddkeminie10@gmail.com>
2026-08-15 15:25:29 +0100

    Add NavBar and Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+function Hero(){
+    return(
+        <>
+        <div className="min-h-screen flex flex-col justify-center p-4 text-center">
+             <p className="font-bold">4.9 Rating over 12,850 Reviews</p>
+            <h2 className="text-3xl md:text-7xl font-calbri max-w-2xl mx-auto leading-tight">Plan more with Twinkle AI</h2>
+            <p className="mt-6">Projects remeber. Agent think. Automations execute.</p>
+            
+                <div className="flex justify-center gap-5 mt-6">
+                    <button className="bg-blue-900 text-white rounded-full px-2 py-2">Get Started</button>
+                    <button className="bg-black text-white rounded-full px-2 py-2">Download APP</button>
+                </div>
+        </div>
+        </>
+    )
+}
+export default Hero;
