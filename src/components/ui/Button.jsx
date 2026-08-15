33e3371a2fcaf09314f896884c301c0a3f1619f4
DirectorofWealth <reverencewealth005@gmail.com>
2026-08-15 23:25:41 +0100

    fix/reusability between files

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,17 +1,10 @@
-export default function Button(){
-
-    return(
-        <div>
-
-        <button
+export default function Button({ children }) {
+    return (
+      <button
         type="button"
-        className="shrink-0 rounded-full bg-black px-6 py-3 text-xs font-medium text-white transition-transform duration-200 ease-in-out 
-        hover:-translate-y-1  cursor-pointer ring focus:ring-1 focus:ring-gray-900 shadow-md"
+        className="shrink-0 rounded-full bg-black px-6 py-3 text-xs font-medium text-white transition-transform duration-200 ease-in-out hover:-translate-y-1 cursor-pointer focus:ring-1 focus:ring-gray-900 shadow-md"
       >
-        Get Your Agent
+        {children}
       </button>
-
-        </div>
-        
-    )
-}
+    );
+  }
