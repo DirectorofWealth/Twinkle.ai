0c05395fee36fe391bcdb49e159c20715f0cfbdf
Ebereonwu-prince-chimdi <chimdiprince6@gmail.com>
2026-08-15 19:36:47 +0100

    I just fixed the component hierarchy

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,23 @@ import ResultsSection from "./components/ResultsSection";
 import BuildWithAI from "./components/BuildWithAI";
 import ClosingCounter from "./components/ClosingCounter";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
-      <StatementBlock />
-      <AgentCTA />
-      <ResultsSection />
-      <BuildWithAI />
-      <ClosingCounter />
+      <Header />
+
+      <main>
+        <Hero />
+        <StatementBlock />
+        <AgentCTA />
+        <ResultsSection />
+        <BuildWithAI />
+        <ClosingCounter />
+      </main>
+
       <Footer />
     </>
   );
